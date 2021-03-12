35f6bcbf20cba9b7f78ba8740a4fd3c805ef943b
Mayank  Tandon <mtandon09@gmail.com>
2021-03-12 12:30:38 -0500

    New config tests script

--- tumor_normal/config_tests/make_rulegraphs.sh
+++ tumor_normal/config_tests/make_rulegraphs.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/bash
 
-configsdir="example_configs"  ## where config jsons are stored
-realconfigfile="references_hg38.json"  ## The filename that the run command will expect
+pipeline_dir="../skeleton/"
 
-pngdir="example_configs/rulegraphs"  ## Where to output rule graph PNGs
+pngdir="$(pwd)/rulegraphs"  ## Where to output rule graph PNGs
 if [ ! -d $pngdir ]; then mkdir -p $pngdir; fi
 
-## Loop through jsons, symlink as "real" config, and generate a rule graph for each
-for json in $configsdir/*.json; do
-    if [ -f $realconfigfile ]; then rm $realconfigfile; fi
-    ln -s $json $realconfigfile
-    
-    jsonlabel=$(echo $(basename $json) | sed -e 's/.json$//')
-    pngfile="$pngdir/rules.$jsonlabel.png"
-    ./run.sh --rulegraph $pngfile
-done
+cd $pipeline_dir
+
+## Default (uses params defined in the references_hg38.json)
+./run.sh --rulegraph "$pngdir/rules.default.png"
+
+## Start from BAM files
+# Since the config file already contains a source fastq parameter, need to override it with a non-existent filepath
+./run.sh --rulegraph "$pngdir/rules.fromBAM.png" --sourcefq "foo" --sourcebam "/data/tandonm/pl_test_data/human/bams"
+
+## Add CNV calling
+# Should be set of one of 'true', 't', or 'yes' (case-insensitive)
+./run.sh --rulegraph "$pngdir/rules.CNV.png" --sourcefq "foo" --cnv "True"
+
+## Add FFPE filtering with SOBDetector
+# Should be set of one of 'true', 't', or 'yes' (case-insensitive)
+./run.sh --rulegraph "$pngdir/rules.FFPE.png" --sourcefq "foo" --ffpe "True"
+
+## Custom set of variant callers
+#  This doesn't work yet; needs to be deleted from the config json!!
+custom_config_str="output_params={'SOMATIC_VCF':{'mutect2':'','mutect':''}}"
+./run.sh --rulegraph "$pngdir/rules.custom_callers.png" --config "$custom_config_str"
+
