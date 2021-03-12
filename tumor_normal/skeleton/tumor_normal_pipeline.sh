0b27c4a75b043b4b43b84767aec60f7a772cad95
Mayank  Tandon <mtandon09@gmail.com>
2021-03-12 12:34:33 -0500

    Syncing back

--- tumor_normal/skeleton/tumor_normal_pipeline.sh
+++ tumor_normal/skeleton/tumor_normal_pipeline.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-module load snakemake; snakemake --stats snakemake.stats --restart-times 0 --rerun-incomplete --cluster "sbatch --gres {cluster.gres} --cpus-per-task {cluster.threads} -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} --job-name={params.rname} -e slurmfiles/slurm-%j_{params.rname}.out -o slurmfiles/slurm-%j_{params.rname}.out" --cluster-config cluster.json --keep-going --snakefile tumor_normal_hg38.snakemake  -j 500
+module load snakemake; snakemake --stats snakemake.stats --restart-times 0 --rerun-incomplete --cluster "sbatch --gres {cluster.gres} --cpus-per-task {cluster.threads} -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} --job-name={params.rname} -e slurmfiles/slurm-%j_{params.rname}.out -o slurmfiles/slurm-%j_{params.rname}.out" --cluster-config cluster.json --keep-going --snakefile tumor_normal_hg38.snakemake   -j 500
  > snakemake.log 2>&1
