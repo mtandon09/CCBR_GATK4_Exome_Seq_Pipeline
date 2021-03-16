def5409200cf8d160ad98e6bd5dc84349d6763cd
Mayank  Tandon <mtandon09@gmail.com>
2021-03-16 16:58:29 -0400

    Bug fixes for tumor_normal

--- tumor_normal/skeleton/run.sh
+++ tumor_normal/skeleton/run.sh
@@ -18,16 +18,13 @@ parser.add_argument('--until',required=False,  default='', help='Rule name to st
 parser.add_argument('--local',required=False,  default='', help='Number of jobs to run in parallel locally; does not submit to slurm, so only use on an interactive node')
 parser.add_argument('--slurmdir',required=False, default='slurmfiles', help='Path to output slurm files to')
 parser.add_argument('--rulegraph',required=False, default='', help='Path to a PNG file to which the rules DAG will be written')
+parser.add_argument('--report',required=False, default='', help='Path to an HTML file to which the snakemake report will be written')
 parser.add_argument('--config',required=False, default='', help='Manually set the \'input_params\' section of the snakemake config file. Overrides any [input_params] arguments.')
 EOF
 
+#SNAKEFILE="tumor_only_hg38.snakemake"
 SNAKEFILE="tumor_normal_hg38.snakemake"
 
-if [ ! -z $UNLOCK ]; then
-    module load snakemake/5.24.1
-    snakemake -j2 --unlock --snakefile $SNAKEFILE --rerun-incomplete
-    exit 0
-fi
 
 untilarg=""
 if [ ! -z $UNTIL ]; then untilarg="--until $UNTIL"; fi
@@ -49,7 +46,13 @@ else
     for i in ${inputparams[@]}; do configarg="$configarg,$i"; done
     if [ ! -z $configarg ]; then configarg="--config \"input_params={$(echo -e $configarg | sed -e 's/^,//')}\""; fi
 fi
-
+if [ ! -z $UNLOCK ]; then
+    module load snakemake/5.24.1
+    echo $configarg
+    mycmd="snakemake -j2 --snakefile $SNAKEFILE --rerun-incomplete $configarg --unlock"
+    eval $mycmd
+    exit 0
+fi
 if [ ! -z $RULEGRAPH ]; then
     module load snakemake/5.24.1
     module load graphviz
@@ -57,6 +60,15 @@ if [ ! -z $RULEGRAPH ]; then
     #echo $mycmd
     eval $mycmd
     echo "Wrote rule graph to $RULEGRAPH. Exiting..."
+    exit 0
+fi
+if [ ! -z $REPORT ]; then
+    module load snakemake/5.24.1
+    module load graphviz
+    mycmd="snakemake --snakefile $SNAKEFILE --rerun-incomplete --report $REPORT $configarg"
+    #echo $mycmd
+    eval $mycmd
+    echo "Wrote report to $REPORT. Exiting..."
     exit 0
 fi
 
@@ -68,7 +80,7 @@ dryrunarg=""
 if [ ! -z $DRYRUN ]; then dryrunarg="-n"; fi
 
 #smk_cmd_base="snakemake --stats snakemake.stats --restart-times 0 --rerun-incomplete -j 500 --cluster "$CLUSTER_OPTS" --cluster-config cluster.json --keep-going --snakefile $SNAKEFILE $untilarg $dryrunarg > snakemake.log 2>&1;"
-smk_cmd_base="module load snakemake/5.24.1; snakemake --stats snakemake.stats --restart-times 0 --rerun-incomplete --cluster \"$CLUSTER_OPTS\" --cluster-config cluster.json --keep-going --snakefile $SNAKEFILE $untilarg $configarg"
+smk_cmd_base="module load snakemake/5.24.1; snakemake --stats snakemake.stats --rerun-incomplete --cluster \"$CLUSTER_OPTS\" --cluster-config cluster.json --keep-going --snakefile $SNAKEFILE $untilarg $configarg"
 
 
 NJOBS="500"
@@ -80,7 +92,7 @@ else
         echo "module load snakemake/5.24.1; snakemake --rerun-incomplete --snakefile $SNAKEFILE -j $LOCAL $untilarg"
         eval "module load snakemake/5.24.1; snakemake --rerun-incomplete --snakefile $SNAKEFILE -j $LOCAL $untilarg"
     else
-        echo -e "#!/usr/bin/bash\n$smk_cmd_base -j $NJOBS\n > snakemake.log 2>&1" > tumor_normal_pipeline.sh
+        echo -e "#!/usr/bin/bash\n$smk_cmd_base -j $NJOBS --restart-times 1 --latency-wait 120 \n > snakemake.log 2>&1" > tumor_normal_pipeline.sh
         echo "Submitting pipeline to cluster... "
         sbatch --cpus-per-task=2 --mem=12g --time 5-00:00:00 --partition ccr,norm --output submit.log --error submit.log tumor_normal_pipeline.sh
     fi
